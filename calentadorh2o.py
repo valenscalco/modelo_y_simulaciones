bd6c325e261d5f117b32530730f1d62c17656846
valenscalco <mv.scalco@hotmail.com>
2023-05-29 02:12:30 -0300

    added

--- calentadorh2o.py
+++ calentadorh2o.py
@@ -1,42 +1,74 @@
 import matplotlib.pyplot as plt
-# Aumento de temperatura
 
-# Sin tener en cuenta la perdida de calor
-m = 1 #Kg, masa del agua
-c = 4186 #J, calor especifico del agua
-Q = 1000 #J, calor entregado por segundo
-temperaturas = [293.15, ] # temperatura inicial 293.15K
 
-while temperaturas[-1] < 373.15:
-    temperaturas.append((Q/(m*c)) + temperaturas[-1])
+class Calentadorh2o:
+    def __init__(self):
+        self.parametros()
 
-print(f"Temperatura final: {temperaturas[-1]}K = {temperaturas[-1] - 273.15}°C")
-print(f"Demora: {len(temperaturas)}s")
+    def parametros(self):
+        self.radio = 0.0535
+        self.altura = 0.15
+        self.espesor = 0.01
+        self.qent = 909
+        self.qesph2o = 4184
+        self.tiempo_max = 400
+        self.ti = 25  # Temperatura inicial
+        self.te = 20  # Temperatura externa
+        self.k = 0.4
 
-eje_x = [segundo for segundo in range(0, len(temperaturas))]
-plt.scatter(eje_x, temperaturas)
-plt.title("Aumento de temperatura sin tener en cuenta la perdida de calor")
-plt.show()
+    def sin_perdidas(self):
+        temperaturas = []
+        tiempos = []
 
-## Teniendo en cuenta la perdida de calor
-Q_resistencia = 1000 #J, calor entregado por segundo por la resistencia
-t_env = 293.15 # kelvin
-k = 0.0004
-A = 0.06845754 # m**2
-d = 0.01 # m
+        for tiempo in range(self.tiempo_max):
+            # Actualiza la temperatura final
+            tf = self.ti + (self.qent / self.qesph2o)
+            # Agrega los valores actuales de temperatura y tiempo a las listas
+            temperaturas.append(tf)
+            tiempos.append(tiempo)
+            # Actualiza la temperatura inicial para la siguiente iteración
+            self.ti = tf
+            # Imprime la temperatura en base a cada segundo
+            # if tiempo == 0 or tiempo == 1 or tiempo == 399:
+            print(f"Tiempo: {tiempo+1} s, Temperatura: {tf} °C")
+        g.grafico_tiempo_temp(tiempos, temperaturas)
 
-temperaturas = [293.15, ] # temperatura inicial 293.15K
+    def con_perdidas(self):
+        # Calcula la temperatura interna de un objeto en un tiempo dado.
+        area = 2 * 3.1416 * self.altura * self.radio + 2 * 3.1416 * self.radio**2
+        temperaturas = []
+        tiempos = []
 
-while temperaturas[-1] < 373.15 and temperaturas[-1] >= temperaturas[0]:
-    Q_perdido = (k*A*(temperaturas[-1] - t_env)) / d
-    Q_real = Q_resistencia - Q_perdido
+        for tiempo in range(self.tiempo_max):
+            perdida_calor = (self.k * area * (self.ti - self.te)) / self.espesor
+            temperaturas.append(self.ti)
+            tiempos.append(tiempo)
+            self.ti += (self.qent - perdida_calor) / self.qesph2o
+            # Imprime la temperatura en base a cada segundo
+            # if tiempo == 0 or tiempo == 1 or tiempo == 399:
+            print(f"Tiempo: {tiempo+1} s, Temperatura: {self.ti} °C")
+        g.grafico_tiempo_temp(tiempos, temperaturas)
 
-    temperaturas.append((Q_real/(m*c)) + temperaturas[-1])
+    # Genera un gráfico de la temperatura en función del tiempo
+    def grafico_tiempo_temp(self, tiempos, temperaturas):
+        x_label = 'Tiempo'
+        y_label = 'Temperatura'
+        title = 'Gráfico de Temperatura vs. Tiempo'
+        plt.plot(tiempos, temperaturas)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.title(title)
+        plt.show()
 
-print(f"Temperatura final: {temperaturas[-1]}K = {temperaturas[-1] - 273.15}°C")
-print(f"Demora: {len(temperaturas)}s")
 
-eje_x = [segundo for segundo in range(0, len(temperaturas))]
-plt.scatter(eje_x, temperaturas)
-plt.title("Aumento de temperatura")
-plt.show()
+if __name__ == "__main__":
+    g = Calentadorh2o()
+    stop = False
+    while not stop:
+        election = int(input("Ingresar 1 para calculo sin perdidas \nIngresar 2 para calculo con perdidas\n"))
+        if election == 1:
+            g.sin_perdidas()
+            stop = True
+        elif election == 2:
+            g.con_perdidas()
+            stop = True
