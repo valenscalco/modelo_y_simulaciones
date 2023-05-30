4302e514f940c2441e9fff8fa1c4d68ca5230f28
valenscalco <mv.scalco@hotmail.com>
2023-05-30 16:05:03 -0300

    arreglado

--- calentadorh2o.py
+++ calentadorh2o.py
@@ -8,7 +8,7 @@ class Calentadorh2o:
     def parametros(self):
         self.radio = 0.0535
         self.altura = 0.15
-        self.espesor = 0.01
+        self.espesor = [0.01, 0.005, 0.05]
         self.qent = 909
         self.qesph2o = 4184
         self.tiempo_max = 400
@@ -31,34 +31,55 @@ class Calentadorh2o:
             # Imprime la temperatura en base a cada segundo
             # if tiempo == 0 or tiempo == 1 or tiempo == 399:
             print(f"Tiempo: {tiempo+1} s, Temperatura: {tf} °C")
-        g.grafico_tiempo_temp(tiempos, temperaturas)
+        g.grafico_tiempo_temp_sin_perdidas(tiempos, temperaturas)
 
     def con_perdidas(self):
         # Calcula la temperatura interna de un objeto en un tiempo dado.
         area = 2 * 3.1416 * self.altura * self.radio + 2 * 3.1416 * self.radio**2
-        temperaturas = []
-        tiempos = []
+        temperaturas1 = []
+        tiempos1 = []
+        temperaturas2 = []
+        tiempos2 = []
 
-        for tiempo in range(self.tiempo_max):
-            perdida_calor = (self.k * area * (self.ti - self.te)) / self.espesor
-            temperaturas.append(self.ti)
-            tiempos.append(tiempo)
-            self.ti += (self.qent - perdida_calor) / self.qesph2o
-            # Imprime la temperatura en base a cada segundo
-            # if tiempo == 0 or tiempo == 1 or tiempo == 399:
-            print(f"Tiempo: {tiempo+1} s, Temperatura: {self.ti} °C")
-        g.grafico_tiempo_temp(tiempos, temperaturas)
+        for i in range(3):
+            temperaturas = []
+            tiempos = []
+            for tiempo in range(self.tiempo_max):
+                perdida_calor = (self.k * area * (self.ti - self.te)) / self.espesor[i]
+                temperaturas.append(self.ti)
+                tiempos.append(tiempo)
+                self.ti += (self.qent - perdida_calor) / self.qesph2o
+                # Imprime la temperatura en base a cada segundo
+                if tiempo == 0 or tiempo == 1 or tiempo == 399:
+                    print(f"Tiempo: {tiempo+1} s, Temperatura: {self.ti} °C")
+            self.ti = 25
+            if i == 0:
+                temperaturas1 = temperaturas
+                tiempos1 = tiempos
+            elif i == 1:
+                temperaturas2 = temperaturas
+                tiempos2 = tiempos
+        g.grafico_tiempo_temp(tiempos, temperaturas, tiempos2, temperaturas2, tiempos1, temperaturas1)
 
     # Genera un gráfico de la temperatura en función del tiempo
-    def grafico_tiempo_temp(self, tiempos, temperaturas):
-        x_label = 'Tiempo'
-        y_label = 'Temperatura'
-        title = 'Gráfico de Temperatura vs. Tiempo'
-        plt.plot(tiempos, temperaturas)
-        plt.xlabel(x_label)
-        plt.ylabel(y_label)
-        plt.title(title)
+    def grafico_tiempo_temp(self, tiempos, temperaturas, tiempos2, temperaturas2, tiempos1, temperaturas1):
+        plt.plot(tiempos1, temperaturas1, label="Espesor = 0.01")
+        plt.plot(tiempos2, temperaturas2, label="Espesor = 0.005")
+        plt.plot(tiempos, temperaturas, label="Espesor = 0.05")
+        plt.xlabel('Tiempo (s)')
+        plt.ylabel('Temperatura (C)')
+        plt.title('Gráfico de Temperatura vs. Tiempo')
+        plt.legend()
         plt.show()
+
+    # Genera un gráfico de la temperatura en función del tiempo
+    def grafico_tiempo_temp_sin_perdidas(self, tiempos, temperaturas):
+        plt.plot(tiempos, temperaturas)
+        plt.xlabel('Tiempo')
+        plt.ylabel('Temperatura')
+        plt.title('Gráfico de Temperatura vs. Tiempo')
+        plt.show()
+        plt.close()
 
 
 if __name__ == "__main__":
